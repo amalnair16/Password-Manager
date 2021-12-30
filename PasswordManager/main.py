2a548c339c1057a5451b3f846f8f0e429c2bf0c9
Amal Nair <amal.nair16@gmail.com>
2021-12-31 03:26:57 +0530

    first commit

---
+++ PasswordManager/main.py
@@ -0,0 +1,9 @@
+pwd = input("What is the master password?")
+
+mode = input("Add new password or view existing password? enter add or view")
+if mode == "view":
+    pass
+elif mode=="add":
+    pass
+else:
+    print("Invalid mode")
