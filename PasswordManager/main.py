a150ccdb82d22814544cfd257d7e9fb4ee96b99d
Amal Nair <amal.nair16@gmail.com>
2021-12-31 03:32:03 +0530

    changes

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,9 +1,10 @@
 pwd = input("What is the master password ?")
 
-mode = input("Add new password or view existing password? please enter add or view")
-if mode == "view":
-    pass
-elif mode=="add":
-    pass
-else:
-    print("Invalid mode")
+while true:
+    mode = input("Add new password or view existing password? please enter add or view")
+    if mode == "view":
+      pass
+    elif mode=="add":
+      pass
+    else:
+      print("Invalid mode")
