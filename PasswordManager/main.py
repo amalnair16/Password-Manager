2118edb004b4900e20aceb152776f54fe5e6536a
Amal Nair <amal.nair16@gmail.com>
2021-12-31 16:24:23 +0530

    add functions add and view updated function add

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,10 +1,18 @@
-pwd = input("What is the master password ?")
-
+master_pwd = input("What is the master password ?")
+def view():
+    pass
+def add():
+    name = input("Account name :")
+    pwd = input("Password :")
+    with open ('password.txt','a') as f:
 while true:
-    mode = input("Add new password or view existing password? please enter add or view")
+    mode = input("Add new password or view existing password? please enter add or view , press q to quit " .lower())
+    if mode=="q":
+        break
     if mode == "view":
-      pass
+       view()
     elif mode=="add":
-      pass
+       add()
     else:
-      print("Invalid mode")
+       print("Invalid mode")
+       continue
