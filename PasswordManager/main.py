b40f0437dc9145d7c0640f7e7e2d814771d4d571
Amal Nair <amal.nair16@gmail.com>
2022-01-02 20:28:41 +0530

    changes

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,21 +1,48 @@
+from cryptography.fernet import Fernet
+
+def write_key():
+    key=Fernet.generate_key()
+    with open("key.key","wb")as key_file:
+        key_file.write(key)
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
 master_pwd = input("What is the master password ?")
+write_key()
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
+
 def view():
-    with open ('password.txt','r') as f:
-       for line in f.readlines():
-           print(line.rstrip())
+    with open('password.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User: ", user, "Password: ",
+                  fer.decrypt(passw.encode()).decode())
+
+
 def add():
     name = input("Account name :")
     pwd = input("Password :")
-    with open ('password.txt','a') as f:
-        f.write(name + "|" + pwd + "\n")
+    with open('password.txt', 'a') as f:
+        f.write(name + "|" + str(fer.encrypt(pwd.encode())) + "\n")
+
+
 while True:
-    mode = input("Add new password or view existing password? please enter add or view , press q to quit " .lower())
-    if mode=="q":
+    mode = input("Add new password or view existing password? please enter add or view , press q to quit ".lower())
+    if mode == "q":
         break
     if mode == "view":
-       view()
-    elif mode=="add":
-       add()
+        view()
+    elif mode == "add":
+        add()
     else:
-       print("Invalid mode")
-       continue
+        print("Invalid mode")
+        continue
